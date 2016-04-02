e2a3793269eb8d7ae67b8b7ebc4bedeb21f2d8e3
ghost3432 <ghost3432@yandex.ru>
2016-04-02 23:18:08 +0300

    Task 5 added

---
+++ task5/Test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <forward_list>
+#include "format.h"
+
+using namespace std;
+
+/*namespace std {
+    class String : public string {
+    public:
+        ~String() {
+            std::string::~basic_string();
+            cout << "String deleted" << endl;
+        }
+    };
+}*/
+
+template <typename T>
+void foo(T arg) {
+    cout << is_array<T>::value;
+}
+
+int main() {
+    int b[8], *a, si;
+    std::cout << typeid(b).name() << " " << typeid(a).name() << " " << typeid(si).name() << endl;
+    std::cout << format("%@ %@ %*d\n", 0.0, (void*)34, 45, 4);
+    printf("%+a, %a, %p\n", 0., 2e-320, (void*)34);
+    std::cout << format("%a\n", 2e-323);
+    std::vector<int> ta;
+    std::forward_list<int> at;
+    foo(b);
+}
