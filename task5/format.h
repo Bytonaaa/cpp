fcc249a2208f16335028371c410c045dbf7c59c5
ghost3432 <ghost3432@yandex.ru>
2016-05-07 19:37:33 +0300

    Task 5: another try

--- task5/format.h
+++ task5/format.h
@@ -144,15 +144,15 @@ namespace formatImpl {
         throw std::invalid_argument("Invalid argument, or this feature is not implemented.");
     }
 
-    template<typename T, typename = typename std::enable_if<std::is_integral<T>::value>::type>
-    int checkForInt(const T &arg) {
+    template<typename T>
+    int checkForInt(const typename std::enable_if<std::is_integral<T>::value, T>::type &arg) {
         if (arg < 0)
             throw std::invalid_argument("Invalid argument: illegal value " + std::to_string(arg));
         return (int) arg;
     }
 
     template <typename T>
-    int checkForInt(...) {
+    int checkForInt(const typename std::enable_if<!std::is_integral<T>::value, T>::type &) {
         throw std::invalid_argument("Invalid format: integral type expected");
     }
 
